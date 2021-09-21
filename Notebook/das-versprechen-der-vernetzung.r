d1f4439e5a5dc3839e413e2375e28ceeb3fded6e
Lukas C. Bossert <lukascbossert@meleager.local>
2021-09-22 01:04:03 +0200

    update notebook with content for workshop

--- Notebook/das-versprechen-der-vernetzung.r
+++ Notebook/das-versprechen-der-vernetzung.r
@@ -1,12 +1,51 @@
+#install.packages('igraph')
 library('igraph')
 
 NFDI_edges <- read.table(header=TRUE,
                          sep=",",
                          text="
 from,to
+DataPLANT,NFDI4BioDiversity
+DataPLANT,NFDI4Chem
+GHGA,NFDI4Health
+KonsortSWD,BERD@NFDI
+KonsortSWD,NFDI4BioDiversity
+KonsortSWD,NFDI4Earth
+KonsortSWD,NFDI4Health
+KonsortSWD,Text+
+NFDI4BioDiversity,NFDI4Earth
+NFDI4BioDiversity,NFDI4Chem
+NFDI4BioDiversity,NFDI4Health
+NFDI4BioDiversity,KonsortSWD
+NFDI4BioDiversity,DataPLANT
+NFDI4Cat,FAIRmat
+NFDI4Cat,NFDI4Chem
+NFDI4Cat,NFDI4Ing
+NFDI4Cat,DAPHNE4NFDI
+NFDI4Chem,FAIRmat
+NFDI4Chem,NFDI4Ing
+NFDI4Chem,NFDI4Cat
+NFDI4Chem,DAPHNE4NFDI
+NFDI4Chem,PUNCH
+NFDI4Chem,NFDI4Health
+NFDI4Chem,NFDI4BioDiversity
+NFDI4Culture,Text+
+NFDI4Culture,MaRDI
+NFDI4Culture,NFDI4Ing
+NFDI4Health,GHGA
+NFDI4Health,KonsortSWD
+NFDI4Health,NFDI4Chem
+NFDI4Health,NFDI4Earth
+NFDI4Health,NFDI4BioDiversity
+NFDI4Ing,NFDI-MatWerk
+NFDI4Ing,FAIRmat
+NFDI4Ing,NFDI4Chem
+NFDI4Ing,NFDI4Earth
+NFDI4Ing,MaRDI
+NFDI4Ing,Text+
+NFDI4Ing,NFDI4Culture
 BERD@NFDI,KonsortSWD
 BERD@NFDI,MaRDI
-BERD@NFDI,NFDI4Memory
 BERD@NFDI,Text+
 DAPHNE4NFDI,FAIRmat
 DAPHNE4NFDI,NFDI-MatWerk
@@ -14,128 +53,74 @@ DAPHNE4NFDI,NFDI4Cat
 DAPHNE4NFDI,NFDI4Chem
 DAPHNE4NFDI,NFDI4Health
 DAPHNE4NFDI,NFDI4Ing
-DAPHNE4NFDI,NFDI4Objects
-DAPHNE4NFDI,PUNCH4NFDI
+DAPHNE4NFDI,PUNCH
 FAIRmat,DAPHNE4NFDI
 FAIRmat,DataPLANT
 FAIRmat,MaRDI
 FAIRmat,NFDI-MatWerk
 FAIRmat,NFDI4Cat
 FAIRmat,NFDI4Chem
-FAIRmat,NFDI4DataScience
+FAIRmat,DataScience
 FAIRmat,NFDI4Ing
-FAIRmat,NFDIxCS
-FAIRmat,PUNCH4NFDI
+FAIRmat,PUNCH
 MaRDI,BERD@NFDI
 MaRDI,FAIRmat
 MaRDI,NFDI-MatWerk
-MaRDI,NFDI-Neuro
 MaRDI,NFDI4Cat
 MaRDI,NFDI4Chem
 MaRDI,NFDI4Ing
-MaRDI,PUNCH4NFDI
+MaRDI,PUNCH
 NFDI-MatWerk,DAPHNE4NFDI
 NFDI-MatWerk,DataPLANT
 NFDI-MatWerk,FAIRmat
 NFDI-MatWerk,MaRDI
 NFDI-MatWerk,NFDI4Chem
-NFDI-MatWerk,NFDI4DataScience
+NFDI-MatWerk,DataScience
 NFDI-MatWerk,NFDI4Ing
-NFDI-MatWerk,NFDIxCS
-NFDI-Neuro,DataPLANT
-NFDI-Neuro,GHGA
-NFDI-Neuro,NFDI4BioDiversity
-NFDI-Neuro,NFDI4Culture
-NFDI-Neuro,NFDI4Earth
-NFDI-Neuro,NFDI4Health
-NFDI-Neuro,NFDI4Ing
-NFDI-Neuro,NFDI4Microbiota
-NFDI4Agri,DataPLANT
-NFDI4Agri,KonsortSWD
-NFDI4Agri,NFDI4BioDiversity
-NFDI4Agri,NFDI4Earth
-NFDI4Agri,NFDI4Health
-NFDI4Agri,NFDI4Immuno
-NFDI4Agri,NFDI4Microbiota
-NFDI4DataScience,KonsortSWD
-NFDI4DataScience,MaRDI
-NFDI4DataScience,NFDI-MatWerk
-NFDI4DataScience,NFDI4BioDiversity
-NFDI4DataScience,NFDI4Cat
-NFDI4DataScience,NFDI4Chem
-NFDI4DataScience,NFDI4Culture
-NFDI4DataScience,NFDI4Health
-NFDI4DataScience,NFDI4Ing
-NFDI4DataScience,NFDI4Microbiota
-NFDI4DataScience,NFDIxCS
+DataScience,KonsortSWD
+DataScience,MaRDI
+DataScience,NFDI-MatWerk
+DataScience,NFDI4BioDiversity
+DataScience,NFDI4Cat
+DataScience,NFDI4Chem
+DataScience,NFDI4Culture
+DataScience,NFDI4Health
+DataScience,NFDI4Ing
+DataScience,NFDI4Microbiota
 NFDI4Earth,DataPLANT
 NFDI4Earth,GHGA
 NFDI4Earth,KonsortSWD
-NFDI4Earth,NFDI4Agri
 NFDI4Earth,NFDI4BioDiversity
 NFDI4Earth,NFDI4Cat
 NFDI4Earth,NFDI4Chem
 NFDI4Earth,NFDI4Culture
 NFDI4Earth,NFDI4Health
 NFDI4Earth,NFDI4Ing
-NFDI4Earth,NFDI4Objects
-NFDI4Immuno,GHGA
-NFDI4Immuno,NFDI4Agri
-NFDI4Immuno,NFDI4Health
-NFDI4Immuno,NFDI4Microbiota
-NFDI4Memory,BERD@NFDI
-NFDI4Memory,KonsortSWD
-NFDI4Memory,MaRDI
-NFDI4Memory,NFDI4Culture
-NFDI4Memory,NFDI4Objects
-NFDI4Memory,Text+
 NFDI4Microbiota,DataPLANT
 NFDI4Microbiota,GHGA
-NFDI4Microbiota,NFDI4Agri
 NFDI4Microbiota,NFDI4BioDiversity
 NFDI4Microbiota,NFDI4Chem
-NFDI4Microbiota,NFDI4DataScience
+NFDI4Microbiota,DataScience
 NFDI4Microbiota,NFDI4Health
-NFDI4Microbiota,NFDI4Immuno
 NFDI4Microbiota,NFDI4Ing
-NFDI4Objects,KonsortSWD
-NFDI4Objects,NFDI4Agri
-NFDI4Objects,NFDI4BioDiversity
-NFDI4Objects,NFDI4Culture
-NFDI4Objects,NFDI4Earth
-NFDI4Objects,NFDI4Memory
-NFDI4Objects,Text+
-NFDI4SD,NFDI4Culture
-NFDI4SD,NFDI4DataScience
-NFDI4SD,NFDI4Memory
-NFDI4SD,NFDI4Objects
-NFDIxCS,FAIRmat
-NFDIxCS,MaRDI
-NFDIxCS,NFDI4Chem
-NFDIxCS,NFDI4DataScience
-NFDIxCS,NFDI4Earth
-NFDIxCS,NFDI4Ing
-PUNCH4NFDI,DAPHNE4NFDI
-PUNCH4NFDI,FAIRmat
-PUNCH4NFDI,GHGA
-PUNCH4NFDI,MaRDI
-PUNCH4NFDI,NFDI4Earth
-PUNCH4NFDI,NFDI4Ing
-PUNCH4NFDI,NFDIxCS
+PUNCH,DAPHNE4NFDI
+PUNCH,FAIRmat
+PUNCH,GHGA
+PUNCH,MaRDI
+PUNCH,NFDI4Earth
+PUNCH,NFDI4Ing
 Text+,KonsortSWD
 Text+,NFDI4BioDiversity
 Text+,NFDI4Culture
 Text+,NFDI4Earth
 Text+,NFDI4Ing
-Text+,NFDI4Memory
-Text+,NFDI4Objects
 ")
 
 NFDI_network <- graph_from_data_frame(NFDI_edges,
                                       directed=FALSE
                                      )
 
-set.seed(1234)
+set.seed(987654)
 
 plot(NFDI_network,                    # loading data frame
      main  = "NFDI-Netzwerk",         # adding a title
@@ -143,7 +128,7 @@ plot(NFDI_network,                    # loading data frame
      )
 
 
-set.seed(1234)
+set.seed(98765)
 
 plot(NFDI_network,                     # loading data frame
      main   = "NFDI-Netzwerk",         # adding a title
@@ -152,7 +137,9 @@ plot(NFDI_network,                     # loading data frame
      )
 
 
-set.seed(1234)
+
+
+set.seed(98765)
 
 
 plot(NFDI_network,                     # loading data frame
@@ -169,7 +156,7 @@ plot(NFDI_network,                     # loading data frame
 
 degree(NFDI_network)                   #* calculate number of edges
 
-set.seed(1234)
+set.seed(98765)
 
 plot(NFDI_network,                     # loading data frame
      main   = "NFDI-Netzwerk",         # adding a title
@@ -189,7 +176,7 @@ NFDI_network_directed <- graph_from_data_frame(NFDI_edges,
                                           directed = TRUE
                                          )
 
-set.seed(1234)
+set.seed(98765)
 
 plot(NFDI_network_directed,            #<<<<<<< loading data frame
      main   = "NFDI-Netzwerk",         # adding a title
@@ -210,7 +197,7 @@ plot(NFDI_network_directed,            #<<<<<<< loading data frame
 degree(NFDI_network_directed,
        mode = "in")
 
-set.seed(1234)
+set.seed(98765)
 
 plot(NFDI_network_directed,            # loading data frame
      main   = "NFDI-Netzwerk (<in>)",  #<<<<<<<< adding a title
@@ -231,7 +218,7 @@ plot(NFDI_network_directed,            # loading data frame
 degree(NFDI_network_directed,
        mode = "out")
 
-set.seed(1234)
+set.seed(98765)
 
 plot(NFDI_network_directed,            # loading data frame
      main   = "NFDI-Netzwerk (<out>)",  #<<<<<<<< adding a title
@@ -255,7 +242,7 @@ NFDI_network_directed_filter <- delete_vertices(NFDI_network_directed,
 degree(NFDI_network_directed_filter,
        mode = "total")
 
-set.seed(1234)
+set.seed(98765)
 
 plot(NFDI_network_directed_filter,           #<<<<<<<< loading data frame
      main   = "NFDI-Netzwerk (<filtered>)",  #<<<<<<<< adding a title
@@ -276,33 +263,26 @@ plot(NFDI_network_directed_filter,           #<<<<<<<< loading data frame
 NFDI_nodes <- read.table(header=TRUE,
                          sep=",",
                          text="
-name,group
-BERD@NFDI,3
-DAPHNE4NFDI,5
-DataPLANT,2
-FAIRmat,5
-GHGA,1
-KonsortSWD,3
-MaRDI,4
-NFDI-MatWerk,4
-NFDI-Neuro,1
-NFDI4Agri,2
-NFDI4BioDiversity,2
-NFDI4Cat,5
-NFDI4Chem,5
-NFDI4Culture,3
-NFDI4DataScience,4
-NFDI4Earth,2
-NFDI4Health,1
-NFDI4Immuno,1
-NFDI4Ing,4
-NFDI4Memory,3
-NFDI4Microbiota,2
-NFDI4Objects,3
-NFDI4SD,3
-NFDIxCS,4
-PUNCH4NFDI,5
-Text+,3
+name,group,round
+DataPLANT,2,1
+GHGA,1,1
+KonsortSWD,3,1
+NFDI4BioDiversity,2,1
+NFDI4Cat,5,1
+NFDI4Chem,5,1
+NFDI4Culture,3,1
+NFDI4Health,1,1
+NFDI4Ing,4,1
+BERD@NFDI,3,2
+DAPHNE4NFDI,5,2
+FAIRmat,5,2
+MaRDI,4,2
+NFDI-MatWerk,4,2
+DataScience,4,2
+NFDI4Earth,2,2
+NFDI4Microbiota,2,2
+PUNCH,5,2
+Text+,3,2
 ")
 
 NFDI_network_directed <- graph_from_data_frame(d = NFDI_edges,        # d = data frame =~ edges
@@ -317,7 +297,9 @@ NFDI_color_code <- c("#f5ac9f", # Medizin
                     )
 NFDI_color_groups <- NFDI_color_code[as.numeric(as.factor(V(NFDI_network_directed)$group))]
 
-set.seed(1234)
+
+
+set.seed(98765)
 
 plot(NFDI_network_directed,            # loading data frame
      main   = "NFDI-Netzwerk (<Konferenzsystematik>)",  #<<<<<<<< adding a title
@@ -335,8 +317,9 @@ plot(NFDI_network_directed,            # loading data frame
      edge.arrow.size    = .5,          # arrow size,  defaults to 1
     )
 
-
-legend("bottomright",   # x-position
+nfdi_plot_legend <- function(){
+    
+    legend("bottomleft",   # x-position
        title  = "NFDI-Konferenzsystematik", # title
        legend = c(
            "(1) Medizin",
@@ -351,84 +334,67 @@ legend("bottomright",   # x-position
        cex    = .75,    # character expansion factor relative to current par("cex").
        pt.cex = 2       # expansion factor(s) for the points
 )
+}
 
-set.seed(1234)
+set.seed(98765)
 
-NFDI_network_directed_cluster <- cluster_optimal(NFDI_network_directed)
-
-
-plot(NFDI_network_directed_cluster,    #<<<<<<<<<<< clustered network data
-     NFDI_network_directed,            # loading data frame
-     main   = "NFDI-Netzwerk (<Konferenzsystematik>)",  # adding a title
+plot(NFDI_network_directed,            # loading data frame
+     main   = "NFDI-Netzwerk (<Konferenzsystematik>)",  #<<<<<<<< adding a title
      frame  = TRUE,                    # making a frame 
      layout = layout.graphopt,         # better layout options
-     #vertex.color       = NFDI_color_groups,   #<<<<<<<<<<  color of nodes
+     vertex.color       = NFDI_color_groups,   #<<<<<<<<<<  color of nodes
      vertex.frame.color = NFDI_color_groups,   #<<<<<<<<<< color of the frame of nodes
      vertex.label.cex   = 0.5,         # size of the description of the labels
      vertex.label.color = "black",     # color of the description 
                                        # color: https://www.w3schools.com/colors/colors_picker.asp 
-     edge.color         = NA,          #<<<<<<<<<<<<<< color of edges
+     edge.color         = "#808080",   # color of edges
      edge.curved        = 0,           # factor of "curvity"
      vertex.size        = degree(NFDI_network_directed,
                                  mode = "total"), #<<<<<<<<<<< size of nodes depends on amount of edges
      edge.arrow.size    = .5,          # arrow size,  defaults to 1
-     col    = NFDI_color_groups,       #<<<<<<<<<<<<<  color of nodes
-     mark.col           = "grey",      #<<<<<<<<<< color groups
-     mark.border        = NA,          #<<<<<<<<<< no border color
     )
+nfdi_plot_legend()
 
-
-legend("bottomright",   # x-position
-       title  = "NFDI-Konferenzsystematik", # title
-       legend = c(
-           "(1) Medizin",
-           "(2) Lebenswissenschaften",
-           "(3) Geisteswissenschaften",
-           "(4) Ingenieurwissenschaften",
-           "(5) Chemie/Physik"
-       ),  # the text of the legend
-       col    = NFDI_color_code ,  # colors of lines and points beside the legend text
-       pch    = 20,     # the plotting symbols appearing in the legend
-       bty    = "n",    # no frame, the type of box to be drawn around the legend (n=no frame)
-       cex    = .75,    # character expansion factor relative to current par("cex").
-       pt.cex = 2       # expansion factor(s) for the points
-)
-
-set.seed(1234)
-
-plot(NFDI_network_directed_cluster,    # clustered network data
-     NFDI_network_directed,            # loading data frame
-     main   = "NFDI-Netzwerk (<Konferenzsystematik>)",  # adding a title
+nfdi_plot_group <- function(NFDI_name) {
+  
+    set.seed(98765)
+    nfdi_local_network <- function(NFDI_name) {
+    plot(NFDI_network_directed,
+     main   = "NFDI-Netzwerk (<Konferenzsystematik>)",  #<<<<<<<< adding a title
+    sub = NFDI_name,
      frame  = TRUE,                    # making a frame 
      layout = layout.graphopt,         # better layout options
-     vertex.frame.color = NFDI_color_groups,   # color of the frame of nodes
+     vertex.color       = NFDI_color_groups,   #<<<<<<<<<<  color of nodes
+     vertex.frame.color = NFDI_color_groups,   #<<<<<<<<<< color of the frame of nodes
      vertex.label.cex   = 0.5,         # size of the description of the labels
      vertex.label.color = "black",     # color of the description 
                                        # color: https://www.w3schools.com/colors/colors_picker.asp 
-     edge.color = c(NA, "#bf4040")[crossing(NFDI_network_directed_cluster, NFDI_network_directed) + 1], 
-                                       #<<<<<<<<<<<<<<<<< show only edges if they go to another group
-     edge.curved        = 0,           # factor of "curvity"
+     edge.curved        = 0.2,           # factor of "curvity"
      vertex.size        = degree(NFDI_network_directed,
                                  mode = "total"), #<<<<<<<<<<< size of nodes depends on amount of edges
      edge.arrow.size    = .5,          # arrow size,  defaults to 1
-     col    = NFDI_color_groups,       #  color of nodes
-     mark.col           = "grey",      # color groups
-     mark.border        = NA,          # no border color
-    )
+         edge.color = with(NFDI_edges,
+                           ifelse(from %in% NFDI_name,"#8b0401",
+                                  ifelse(to == NFDI_name,"#01888B",
+                                         NA)))
+        )
+    nfdi_plot_legend()
+
+      
+          }
+    
+
+# pdf(paste0("img/network_group_",NFDI_name,".pdf"))   
+# nfdi_local_network(NFDI_name)
+# dev.off()
+ nfdi_local_network(NFDI_name)
+}
+nfdi_plot_group("NFDI4Ing")
+
+nfdi_plot_group("NFDI4Microbiota")
+
+for (name in NFDI_nodes$name){
+  nfdi_plot_group(name)
+}
 
 
-legend("bottomright",   # x-position
-       title  = "NFDI-Konferenzsystematik", # title
-       legend = c(
-           "(1) Medizin",
-           "(2) Lebenswissenschaften",
-           "(3) Geisteswissenschaften",
-           "(4) Ingenieurwissenschaften",
-           "(5) Chemie/Physik"
-       ),  # the text of the legend
-       col    = NFDI_color_code ,  # colors of lines and points beside the legend text
-       pch    = 20,     # the plotting symbols appearing in the legend
-       bty    = "n",    # no frame, the type of box to be drawn around the legend (n=no frame)
-       cex    = .75,    # character expansion factor relative to current par("cex").
-       pt.cex = 2       # expansion factor(s) for the points
-)
